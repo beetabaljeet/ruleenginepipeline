1d610ac89322e5ae4499b29c7ce31d50a108bd71
beetabaljeet <36457090+beetabaljeet@users.noreply.github.com>
2024-11-15 18:06:17 +0530

    feat-initial-commit
    feat-initial-commit

---
+++ ruleenginepipline/src/main/java/com/ruleenginepipline/RuleenginepiplineApplication.java
@@ -0,0 +1,13 @@
+package com.ruleenginepipline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuleenginepiplineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RuleenginepiplineApplication.class, args);
+	}
+
+}
---
+++ ruleenginepipline/src/test/java/com/ruleenginepipline/RuleenginepiplineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ruleenginepipline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuleenginepiplineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
